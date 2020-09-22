65e265e76bbccac5a2c59a3813a36e33e6b8f55a
vikto <vikto@DESKTOP-8O7VMPB>
2020-09-22 10:27:44 +0200

    add 100 csillag

---
+++ orai_09_22/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orai_09_22
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Weinber Viktor");
+
+            Console.ReadKey(true);
+        }
+    }
+}
