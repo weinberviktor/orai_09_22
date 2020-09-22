3a6217fc7d296cc1d07af9651455ecdc6247366e
vikto <vikto@DESKTOP-8O7VMPB>
2020-09-22 10:32:35 +0200

    100

--- orai_09_22/Program.cs
+++ orai_09_22/Program.cs
@@ -8,11 +8,17 @@ namespace orai_09_22
 {
     class Program
     {
+        static Random rnd = new Random();
         static void Main(string[] args)
         {
             Console.ForegroundColor = ConsoleColor.Red;
             Console.WriteLine("Weinber Viktor");
-
+            for (int i = 0; i < 100; i++)
+            {
+                Console.SetCursorPosition(rnd.Next(Console.WindowHeight),
+                    rnd.Next(Console.WindowWidth));
+                Console.Write("*");
+            }
             Console.ReadKey(true);
         }
     }
